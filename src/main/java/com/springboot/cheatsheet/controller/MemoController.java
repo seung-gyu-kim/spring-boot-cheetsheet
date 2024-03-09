985c05afb1c283f10b16979f37616db381ec1a55
김승규 <rlatmdrb1996@gmail.com>
2024-03-09 23:53:04 +0900

    24.03.09(토): 컨트롤러

---
+++ src/main/java/com/springboot/cheatsheet/controller/MemoController.java
@@ -0,0 +1,30 @@
+package com.springboot.cheatsheet.controller;
+
+import com.springboot.cheatsheet.dto.MemoResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class MemoController {
+    //    {
+//        "title": "Controller와 RestController의 차이",
+//        "content": "Controller는 View를, RestController는 JSON을 반환한다.",
+//        "primary": "보통",
+//        "author": "김승규",
+//        "createdAt": "2024-03-09T11:27:51",
+//        "updatedAt": "2024-03-10T12:00:00"
+//    }
+    @GetMapping("/")
+    public MemoResponseDto getMemo() {
+        return new MemoResponseDto(
+                "Controller와 RestController의 차이",
+                "Controller는 View를, RestController는 JSON을 반환한다.",
+                "보통",
+                "김승규",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+}
