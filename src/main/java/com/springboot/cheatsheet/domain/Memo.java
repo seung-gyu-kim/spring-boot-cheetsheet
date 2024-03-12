54009b3a3e522fe09a880ebb8876dacf793becbd
김승규 <rlatmdrb1996@gmail.com>
2024-03-12 21:11:31 +0900

    24.03.12(화): Entity

--- src/main/java/com/springboot/cheatsheet/domain/Memo.java
+++ src/main/java/com/springboot/cheatsheet/domain/Memo.java
@@ -1,17 +1,28 @@
 package com.springboot.cheatsheet.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
+@Entity
 public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "memo_id", nullable = false)
+    private Long id;
+
+    @Column(name = "memo_title")
     private String title;
+
+    @Column(name = "memo_content")
     private String content;
+
+    @Column(name = "memo_primary")
     private String primary;
+
+    @Column(name = "memo_author")
     private String author;
+
     private LocalDateTime createdAt;
+
     private LocalDateTime updatedAt;
 }
